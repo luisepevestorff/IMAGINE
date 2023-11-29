b845b4af0ae5bf8fa9e4b000f59c34ffb070b12a
luisepevestorff <luisepevestorff@hotmail.de>
2023-11-29 11:01:29 +0100

    added comments & to do's

--- check_file_format.py
+++ check_file_format.py
@@ -2,14 +2,13 @@
 
 import os
 
+path_to_file = "/Users/luise/Pictures/Logos/Logo2.png" #going to be based on input
+
 def file_format(path):
     name, format  = os.path.splitext(path)
     return (format)
 
-path_to_file = "/Users/luise/Pictures/Logos/Logo2.png"
-format = file_format(path_to_file)
+format = file_format(path_to_file) #to do: merge with line below
 format_lowercase = format.lower()
 if format_lowercase != ".jpg" and format_lowercase != ".png" and format_lowercase != ".tif":
-    print("Unsupported file format!")
-
-print(format)
+    print("Unsupported file format!") #to do: add more formats?
