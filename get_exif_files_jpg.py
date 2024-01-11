2346e4c4ca27671c123b273768e8ce7d08c5437a
luisepevestorff <luisepevestorff@hotmail.de>
2024-01-11 12:30:01 +0100

    cleaned up and made it work autonomously

--- get_exif_files_jpg.py
+++ get_exif_files_jpg.py
@@ -1,53 +1,64 @@
 import imageio.v2 as imageio
 import re
-from PIL import Image, ExifTags
+from PIL import Image
+import os
+from check_file_format import FileFormat
 
-path = #this is where the path is read in
+print("Welcome to IMAGINE - the singlee, your frienly package to display all the information you ever wanted to know about your favourite image!")
+image_name = input("Please type in what image you want to know everything about. ")
 
-image = imageio.imread(path)
-type = Image.open(path)
+current_directory = os.getcwd()
+path = current_directory+ "/" + image_name
+format = FileFormat.file_format(path)
 
-megapixels = round((type.size[0]*type.size[1]/1000000))
-d = re.sub(r'[a-z]', '', str(image.dtype))
-t = len(Image.Image.getbands(type))
-width = type.size[0]
-height = type.size[1]
+if format != ".jpg" and format != ".tif":
+    raise ValueError("Hello World, this file format is not yet supported (we're working on that). Please choose a JPG or TIF file.")
+else:
+    image = imageio.imread(path)
+    attributes = Image.open(path)
 
-def aspectRatio(w, h):
-    ratio = w/h
-    if ratio.is_integer():
-       resultA = w/100
-       resultB = h/100
-    else:
-        for i in range(1,100):
-            r = i*ratio
-            if r.is_integer():
-                resultA = i
-                resultB = int(r)
-                break
-    return resultB, resultA
+    width = attributes.width
+    height = attributes.height
+    megapixels = round((width*height/1000000))
+    depth = re.sub(r'[a-z]', '', str(image.dtype))
+    channels = len(Image.Image.getbands(attributes))
 
-def aspectRatioRounded(w, h):
-    ratio = w/h
-    if ratio.is_integer():
-       resultA = w/100
-       resultB = h/100
-    else:
-        for i in range(1,100):
-            r = round(i*ratio)
-            if r.is_integer():
-                resultA = i
-                resultB = int(r)
-                break
-    return resultB, resultA
+    def aspectRatio(w, h):
+        ratio = w/h
+        if ratio.is_integer():
+            resultA = w/100
+            resultB = h/100
+        else:
+            for i in range(1,int(1e6)):
+                r = i*ratio
+                if r.is_integer():
+                    resultA = i
+                    resultB = int(r)
+                    break
+        return resultB, resultA
 
-print('Format: ', type.format)
-print('Width: ', width)
-print('Height: ', height)
-print('Bit Depth per Channel: ', d)
-print('Bit Depth per Pixel: ', int(d)*int(t))
-print('Number of Channels: ', t)
-print('Aspect Ratio: ', aspectRatio(width, height))
-print('Aspect Ratio rounded: ', aspectRatioRounded(width, height) )
-print('Megapixels: ', megapixels)
-print('Mode: ', type.mode)
+    def aspectRatioRounded(w, h):
+        ratio = w/h
+        if ratio.is_integer():
+            resultA = w/100
+            resultB = h/100
+        else:
+            for i in range(1,int(1e6)):
+                r = round(i*ratio)
+                if r.is_integer():
+                    resultA = i
+                    resultB = int(r)
+                    break
+        return resultB, resultA
+
+    print("You chose " + image_name + "! Here is all I know about your chosen file:" )
+    print('Format: ', format)
+    print('Width: ', width)
+    print('Height: ', height)
+    print('Bit Depth per Channel: ', depth)
+    print('Bit Depth per Pixel: ', int(depth)*int(channels))
+    print('Number of Channels: ', channels)
+    print('Aspect Ratio: ', aspectRatio(width, height))
+    print('Aspect Ratio rounded: ', aspectRatioRounded(width, height))
+    print('Megapixels: ', megapixels)
+    print('Mode: ', attributes.mode)
