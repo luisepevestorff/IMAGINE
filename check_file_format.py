8e69f150ae67ffe6d9c0c298ca9103cc61e2a6b5
luisepevestorff <luisepevestorff@hotmail.de>
2024-01-11 12:46:01 +0100

    created class and updated function

--- check_file_format.py
+++ check_file_format.py
@@ -2,14 +2,23 @@
 
 import os
 
-def file_format(path):
-    name, format  = os.path.splitext(path)
-    return (format)
+class FileFormat:
+    """Check for file format
 
-path_to_file = "/Users/luise/Pictures/Logos/Logo2.png"
-format = file_format(path_to_file)
-format_lowercase = format.lower()
-if format_lowercase != ".jpg" and format_lowercase != ".png" and format_lowercase != ".tif":
-    print("Unsupported file format!")
+    Args:
+        path: 
+    
+    """
+    #def __init__(self, path):
+        #self.path = path
 
-print(format)
+    def file_format(path):
+        name, file_extension  = os.path.splitext(path)
+        format_lower = file_extension.lower()
+        return(format_lower)
+    
+#path_to_file = "/Users/luise/Pictures/Logos/Logo2.png"
+#format = file_format(path_to_file)
+#format_lowercase = format.lower()
+#if format_lowercase != ".jpg" and format_lowercase != ".png" and format_lowercase != ".tif":
+#    print("Unsupported file format!")
