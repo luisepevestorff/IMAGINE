201def39d4743bc352873579568bd97e4d761df4
luisepevestorff <luisepevestorff@hotmail.de>
2024-01-25 14:30:27 +0100

    adapted to renamed file get_image_data.py

--- IMAGINE/IMAGINE_SingleEdition.py
+++ IMAGINE/IMAGINE_SingleEdition.py
@@ -1,61 +1,11 @@
 #!/usr/bin/python
 
 import imageio.v2 as imageio
-#import re
 from PIL import Image
 import os
 from check_file_format import FileFormat
-from get_exif_files import JpgTif
+from get_image_data import JpgTif
 
-'''
-def aspectRatio(w, h):
-        """Aspect ratio calculation
-        Calculate the aspect ratio of the image.
-
-        Args:
-            w (integer): the width of the image.
-            h (integer): the height of the image.
-        
-        Returns:
-            two integers: the aspect ratio of the image, width to height.
-        """
-        ratio = w/h
-        if ratio.is_integer():
-            resultA = w/100
-            resultB = h/100
-        else:
-            for i in range(1,int(1e6)):
-                r = i*ratio
-                if r.is_integer():
-                    resultA = i
-                    resultB = int(r)
-                    break
-        return resultB, resultA
-
-def aspectRatioRounded(w, h):
-    """Aspect ratio calculation rounded
-    Calculate the aspect ratio of the image rounded to the nearest integer.
-
-    Args:
-        w (integer): the width of the image.
-        h (integer): the height of the image.
-        
-    Returns:
-        two integers: the rounded aspect ratio of the image, width to height.
-    """
-    ratio = w/h
-    if ratio.is_integer():
-        resultA = w/100
-        resultB = h/100
-    else:
-        for i in range(1,int(1e6)):
-            r = round(i*ratio)
-            if r.is_integer():
-                resultA = i
-                resultB = int(r)
-                break
-    return resultB, resultA
-    '''
 
 if __name__ == '__main__':
     print("Welcome to IMAGINE - the single edition , your frienly package to display all the information you ever wanted to know about your favourite image!")
@@ -65,7 +15,6 @@ if __name__ == '__main__':
     path = current_directory+ "/" + image_name
     format = FileFormat(path).file_format()
 
-    
     image = imageio.imread(path)
     attributes = Image.open(path)
 
