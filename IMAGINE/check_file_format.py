fbe9376c25ce3fe54bb5b5e49757b03956a7fe51
luisepevestorff <luisepevestorff@hotmail.de>
2024-01-25 14:23:55 +0100

    added exception

--- IMAGINE/check_file_format.py
+++ IMAGINE/check_file_format.py
@@ -3,20 +3,25 @@
 import os
 
 class FileFormat:
-    """Check for the file format of the image.
-
-    Args:
-        path (string): the path to the desired file.
     
-    Returns: string: the file format of the image displayed in lower case letters.
-    """
     def __init__(self, path):
         self.path = path
 
     def file_format(self):
+        """File format check.
+        Checks for the file format of the image.
+
+        Args:
+            path (string): the path to the desired file.
+    
+        Returns: 
+            string: the file format of the image displayed in lower case letters.
+        """
         name, file_extension  = os.path.splitext(self.path)
         format_lower = file_extension.lower()
-        return(format_lower)
-#if format_lowercase != ".jpg" and format_lowercase != ".png" and format_lowercase != ".tif":
-#print("Unsupported file format!")
-
+        
+        if format_lower != ".jpg" and format_lower != ".tif":
+            raise ValueError("Hello World, this file format is not yet supported (we're working on that). Please choose a JPG or TIF file.")
+        else:
+            format = format_lower
+        return(format)
