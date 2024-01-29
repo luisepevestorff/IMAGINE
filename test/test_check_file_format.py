816a3809d76de402eb88273caf68256d399f8145
luisepevestorff <luisepevestorff@hotmail.de>
2024-01-29 15:33:04 +0100

    fixed test case

--- test/test_check_file_format.py
+++ test/test_check_file_format.py
@@ -7,11 +7,11 @@ def test_file_format():
     
     testformat_2 = FileFormat('./testfile_tif.tif')
     extension_2 = testformat_2.file_format()
-    
-    assert extension_1 == '.jpg'
-    assert extension_2 == '.tif'
-    
+
     with pytest.raises(ValueError) as wrongformat:
         testformat_3 = FileFormat('./testfile_png.png')
         extension_3 = testformat_3.file_format()
     assert str(wrongformat.value) == "Hello World, this file format is not yet supported (we're working on that). Please choose a JPG or TIF file."
+        
+    assert extension_1 == '.jpg'
+    assert extension_2 == '.tif'
